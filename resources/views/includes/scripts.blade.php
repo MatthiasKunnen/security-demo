a59aa6b43340c3d2ff797ab346c07399768ef4bf
Matthias Kunnen <matthias.kunnen@gmail.com>
2016-12-04 21:56:42 +0200

    Added script to automatically resize textareas

--- resources/views/includes/scripts.blade.php
+++ resources/views/includes/scripts.blade.php
@@ -1,1 +1,7 @@
+<script src="/js/text-area-autogrow.min.js"></script>
+<script>
+    jQuery(function() {
+        jQuery('.textarea-auto-size').autogrow({vertical: true, horizontal: false});
+    });
+</script>
 @yield('scripts')
