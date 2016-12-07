57c704cfe9b2ba23a39fe21d63b9ff3261d8ca1d
Matthias Kunnen <matthias.kunnen@gmail.com>
2016-12-07 05:29:38 +0200

    Implemented SQL injection vulnerable search

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\PostRequest;
 use App\Models\Post;
+use App\ViewModels\PostSearchResultViewModel;
 use App\ViewModels\PostViewModel;
+use DB;
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 
 class PostController extends Controller
 {
@@ -42,12 +46,56 @@ class PostController extends Controller
     /**
      * Shows all posts.
      *
+     * @param Request $request
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function showAll()
+    public function showAll(Request $request)
     {
-        return view('welcome', ['posts' => Post::all()->map(function ($item) {
+        if ($request->has('query')) {
+            return $this->find($request);
+        }
+        return view('welcome', ['posts' => $this->getAllPosts()]);
+    }
+
+    /**
+     * Find posts with certain content.
+     *
+     * @param FormRequest $request The request containing the keywords that need to be found.
+     */
+    public function find(Request $request)
+    {
+        if (!$request->has('query')) {
+            return $this->showAll($request);
+        }
+
+        $result = DB::select('SELECT id, created_at, title, content FROM posts WHERE content LIKE "%' . $request->get('query') . '%"');
+        $returnedResults = new Collection();
+
+        foreach ($result as $rowObject) {
+            $row = (array)$rowObject;
+            $post = new PostSearchResultViewModel();
+            $post->id = $row['id'];
+            $post->created_at = $row['created_at'];
+            $post->title = $row['title'];
+            $post->content = $row['content'];
+            $returnedResults->push($post);
+        }
+
+        return view('welcome', [
+            'posts' => $this->getAllPosts(),
+            'search_results' => $returnedResults,
+        ]);
+    }
+
+    /**
+     * Returns all posts in a PostViewModel format.
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    protected function getAllPosts()
+    {
+        return Post::all()->map(function ($item) {
             return new PostViewModel($item);
-        })]);
+        });
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,43 @@
 @section('title', trans('welcome.title'))
 
 @section('content')
+    <div class="row clearfix">
+        <form class="form col-xs-12">
+            <div class="form-group col-md-11">
+                <label class="sr-only">{{ trans('post.search_a_post') }}</label>
+                <input type="text" class="form-control" name="query" id="query"
+                       placeholder="{{ trans('post.search_a_post') }}">
+            </div>
+            <button class="btn btn-default btn-primary col-md-1">
+                {{ trans('post.search') }}
+            </button>
+        </form>
+    </div>
+
+    @if(!empty($search_results))
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>{{ trans('post.id') }}</th>
+                    <th>{{ trans('post.created_at') }}</th>
+                    <th>{{ trans('post.title') }}</th>
+                    <th>{{ trans('post.content') }}</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($search_results as $post)
+                    <tr>
+                        <td><a href="/post/{{ $post->id }}">{{ $post->id }}</a></td>
+                        <td>{{ $post->created_at }}</td>
+                        <td>{{ $post->title }}</td>
+                        <td>{{ $post->content }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+    @endif
+
     <h1 class="page-title">{{trans_choice('post.post', 2)}}</h1>
     @each('includes.post', $posts, 'post')
 @stop
